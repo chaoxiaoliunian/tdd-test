1d28428475b3c39c097fff1f3cc7bced95c4e86f
qishaojun <1013988871@qq.com>
2024-02-29 13:54:42 +0800

    TDD推进至第4章，利用equals解决amount的私有性问题，优化现有的测试

--- src/test/java/test/Test4.java
+++ src/test/java/test/Test4.java
@@ -1,34 +1,33 @@
 package test;
 
-
 import org.junit.Test;
-import tdd.test.test3.Dollar;
+import tdd.test.test4.Dollar;
 
 import static org.junit.Assert.*;
-
 /**
  * 当瑞士法郎与美元的汇率为2：1,5美元+10瑞士法郎=10美元
  * 5美元*2=10美元
- * 将amount定义为私有
+ * 将amount定义为私有 done
  * Dollar副作用——外部操作可以直接改变类的状态？ done
  * 钱必须整数？
  * 实现equals函数，这样就不需要访问amount了，就能解决私有问题。（值对象必须实现equals函数，且值对象不能有副作用，就像两张钱一样，不能改5美元为10美元） done
  * 考虑到hash表，实现equals函数还需要实现hashCode函数
  */
-public class Test3 {
+public class Test4 {
+    /**
+     *  5美元*2=10美元
+     */
     @Test
     public void testMultiplication() {
-        tdd.test.test2.Dollar five = new tdd.test.test2.Dollar(5);
-        tdd.test.test2.Dollar product=five.times(2);
-        assertEquals(10, product.amount);
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
         //测试是否有副作用即 5*2后计算5*3的结果是15，后一步不受之前的影响，即外部的操作不影响类的内部状态
         /**
          * 将一个设计缺陷转化为一个由此缺陷导致运行失败的测试程序
          * 采用存根实现代码快速编译通过（这里的存根不太理解，我理解的就是补充缺失的类）
          * 键入代码以使程序能尽快工作
          */
-        product=five.times(3);
-        assertEquals(15,product.amount);
+        assertEquals(new Dollar(15), five.times(3));
     }
     @Test
     public void testEquals() {
@@ -44,5 +43,4 @@ public class Test3 {
          */
         assertFalse(five.equals(new Dollar(6)));
     }
-
 }
