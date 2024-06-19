a20387bf7d598dd4b98d371a41375add0a219105
sjqi <1013988871@qq.com>
2024-06-20 00:00:50 +0800

    feat: 补充打包和命令例子

---
+++ test-doc/readme.md
@@ -0,0 +1,1 @@
+java -jar .\test-doc-1.0-SNAPSHOT-shaded.jar E:\workspace\tdd-test\test-doc\src\test\java\org\example\PlyWoodMatchTest2.java d:/test2.xlsx 齐少军 2024/06/19 v0.1
---
+++ test-doc/src/test/java/org/example/PlyWoodMatchTest2.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class PlyWoodMatchTest2 extends TestCase {
+    /**
+     * 被测试类
+     */
+    PlyWoodMatch plyWoodMatch = new PlyWoodMatch();
+
+    /**
+     * 测试配对日
+     * 目的:
+     * 测试胶合板每日选择交割正常配对
+     * 期望结果:
+     * 给定买卖方都正常配对
+     */
+    @Test
+    public void testMatch01() {
+    }
+
+    /**
+     * 测试配对日02
+     * 目的:
+     * 测试胶合板每日选择交割被动选择配对
+     * 期望结果:
+     * 被动选择的买方都正常配对
+     */
+    @Test
+    public void testMatch02() {
+    }
+
+    /**
+     * 测试配对日存储
+     * 目的:
+     * 测试配对结果正常存储
+     * 期望结果:
+     * 存储结果都正确
+     */
+    @Test
+    public void testSaveMatch() {
+    }
+
+    /**
+     * 测试什么都不做
+     * 目的:
+     * 什么都不做
+     * 期望结果:
+     * 什么都不做
+     * 单纯测试
+     */
+    @Test
+    public void none() {
+
+    }
+}
